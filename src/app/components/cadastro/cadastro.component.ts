8a54dad0b6531efe9025aba469edaa1e538d79cb
Renan Lopes <rrloppes7@gmail.com>
2026-02-02 16:02:50 -0300

    feat: implement cadastro component with form validation and image assets

--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrl: './cadastro.component.scss'
 })
 export class CadastroComponent {
+
+  name: string = '';
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+
+  constructor(private auth: AuthService) { }
+
+  validateForm(): boolean {
+    return this.name !== '' &&
+      this.email !== '' &&
+      this.password !== '' &&
+      this.confirmPassword !== '';
+  }
+
+  cadastrar() {
+    if (this.validateForm()) {
+      this.auth.cadastro(this.name, this.email, this.password, this.confirmPassword)
+    } else {
+      alert('Preencha todos os campos');
+    }
+  }
 
 }
