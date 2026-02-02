701c30a85936bfe2766252b105908cf0237e1ee1
Renan Lopes <rrloppes7@gmail.com>
2026-02-02 15:58:58 -0300

    basics configs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
 
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { environment } from '../assets/environments/environment';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { environment } from '../environments/environment';
+
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { RecuperarSenhaComponent } from './components/recuperar-senha/recuperar-senha.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    CadastroComponent,
+    RecuperarSenhaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'black': '#000000',
+      },
+    },
+  },
+  plugins: [],
+}
+
+
