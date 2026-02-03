bf4a8ee48379665b6fe0d85da88d08235a463dee
Renan Lopes <rrloppes7@gmail.com>
2026-02-03 10:18:54 -0300

    feat: implementar tela de login e recuperação de senha com validação de formulário

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { RecuperarSenhaComponent } from './components/recuperar-senha/recuperar-senha.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path: '', 
     component: LoginComponent
+  },
+  {
+    path: 'cadastro',
+    component: CadastroComponent
+  },
+  {
+    path: 'recuperar-senha',
+    component: RecuperarSenhaComponent
   }
 ];
 
